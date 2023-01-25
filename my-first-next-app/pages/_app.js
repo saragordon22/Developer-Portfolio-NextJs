83680fede0e1b60e198cca5b05f20ee1784ce0fe
saragordon22 <113549599+saragordon22@users.noreply.github.com>
2023-01-25 23:16:55 +0000

    Update _app.js

--- my-first-next-app/pages/_app.js
+++ my-first-next-app/pages/_app.js
@@ -1,15 +1,30 @@
 import Navbar from "../components/Navbar";
 import Layout from '../components/Layout';
+import Script from 'next/script';
 import '../styles/globals.css' 
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+<Script 
+async src="https://www.googletagmanager.com/gtag/js?id=G-GK5NMLQN9Q"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GK5NMLQN9Q');
+  `}
+</Script>
     <div>
       <Navbar />
        <Layout>
       <Component {...pageProps} />
       </Layout>
     </div>
+    </>
   );
 }
 
